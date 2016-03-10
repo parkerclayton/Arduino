6f06370e74db53ada4a40906a399eb3664ae8a4d
parkerclayton@comcast.net <parkerclayton@comcast.net>
2016-03-09 23:58:05 -0800

    did calcs with the rpm and delaylength python files

--- delaylength.py
+++ delaylength.py
@@ -17,3 +17,27 @@ def dl(rpm):
   #notes
 
   """ http://www.atmel.com/images/doc8017.pdf"""
+
+"""
+Current Sequence
+
+1. A forward
+2. B Backwards
+3. B forwards
+4. A backwards
+
+Suggested Sequence from schematic
+
+0. A and B forward
+1. A backwards, B forwards
+2. A backwards, B backwards
+3. A forwards, B backwards
+4. A forwards, B forwards
+
+datasheet says 60Hz
+so..."""
+
+#suggested rpm is 18, which makes delaylength 185.18
+#will try it tomorrow
+#stackexchange suggests that if cycling through steps too fast, motor will just
+#sit there and vibrate
--- rpm.py
+++ rpm.py
@@ -3,6 +3,7 @@
 
 alpha = 1.8 #degrees
 spr =200.00
+f= 60.00 #Hz value obtained from data sheet
 
 
 def rpm(dl):
@@ -11,6 +12,13 @@ def rpm(dl):
   rpm=(spr*StepsPerSecond*60.00)
   print('stepper rpm: %s') %rpm
 
+  #alternative
+  rpm1=((f*alpha)/360.00)*60.00
+  print('stepper rpm from suggested frequency: %s') %rpm1
+
   #notes
 
   """ http://www.atmel.com/images/doc8017.pdf"""
+
+
+#suggested rpm is 18 which seems about right
