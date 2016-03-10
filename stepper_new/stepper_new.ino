1cd7de6118e4e37afd7f695f480dbb4710f08ccf
parkerclayton@comcast.net <parkerclayton@comcast.net>
2016-03-10 00:12:55 -0800

    updated stepper new script for appropriate sequence and delay length. should be the first thing tested tomorrow

--- stepper_new/stepper_new.ino
+++ stepper_new/stepper_new.ino
@@ -8,14 +8,14 @@ http://www.instructables.com/id/Arduino-Motor-Shield-Tutorial/
 
 *************************************************************/
 
-int delayt = 200;
+int delayt = 185;
 
 void setup() {
-  
+
   //establish motor direction toggle pins
   pinMode(12, OUTPUT); //CH A -- HIGH = forwards and LOW = backwards???
   pinMode(13, OUTPUT); //CH B -- HIGH = forwards and LOW = backwards???
-  
+
   //establish motor brake pins
   pinMode(9, OUTPUT); //brake (disable) CH A
   pinMode(8, OUTPUT); //brake (disable) CH B
@@ -33,7 +33,7 @@ void loop(){
   digitalWrite(9, HIGH);  //DISABLE CH A
   digitalWrite(8, LOW); //ENABLE CH B
   digitalWrite(13, HIGH);   //Sets direction of CH B
-  analogWrite(11, 255);   //Moves CH B 
+  analogWrite(11, 255);   //Moves CH B
   delay(delayt);
 
 //A backwards
@@ -45,15 +45,15 @@ void loop(){
   digitalWrite(9, HIGH);  //DISABLE CH A
   digitalWrite(8, LOW); //ENABLE CH B
   digitalWrite(13, HIGH);   //Sets direction of CH B
-  analogWrite(11, 255);   //Moves CH B 
+  analogWrite(11, 255);   //Moves CH B
   delay(delayt);
-  
+
 //A backwards
   digitalWrite(9, LOW);  //ENABLE CH A
   digitalWrite(8, HIGH); //DISABLE CH B
   digitalWrite(12, LOW);   //Sets direction of CH A
-  analogWrite(3, 255);   //Moves CH A   
-// B backwards  
+  analogWrite(3, 255);   //Moves CH A
+// B backwards
   digitalWrite(9, HIGH);  //DISABLE CH A
   digitalWrite(8, LOW); //ENABLE CH B
   digitalWrite(13, LOW);   //Sets direction of CH B
@@ -65,14 +65,12 @@ void loop(){
   digitalWrite(8, HIGH); //DISABLE CH B
   digitalWrite(12, HIGH);   //Sets direction of CH A
   analogWrite(3, 255);   //Moves CH A
-// B backwards  
+// B backwards
   digitalWrite(9, HIGH);  //DISABLE CH A
   digitalWrite(8, LOW); //ENABLE CH B
   digitalWrite(13, LOW);   //Sets direction of CH B
   analogWrite(11, 255);   //Moves CH B
   delay(delayt);
-  
+
 
 }
-
-
